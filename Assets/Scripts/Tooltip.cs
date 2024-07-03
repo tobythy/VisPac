031e2ac42904fdecc68447fc442db922aef65cf4
Leon Li Ang <49157155+LeonLiAng929@users.noreply.github.com>
2024-07-03 11:34:49 +1000

    MadeToolTip Hoverable

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -11,7 +11,7 @@ public class Tooltip : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
